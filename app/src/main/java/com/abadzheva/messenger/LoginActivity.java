a494bd9044a243115fd11994a8e1d08edaedb44d
k8abadzheva <make.me.loco@gmail.com>
2024-06-26 03:21:52 +0300

    feat: Initial setup
    Set up the basic project structure, including:
    
    - Added launcher icons and background color.
    - Created a LoginActivity with a simple
     login layout.
    - Updated dependencies for testing.

--- app/src/main/java/com/abadzheva/messenger/LoginActivity.java
+++ app/src/main/java/com/abadzheva/messenger/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.abadzheva.messenger;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewForgotPassword;
+    private TextView textViewRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +26,32 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(
+                    systemBars.left + v.getPaddingLeft(),
+                    systemBars.top + v.getPaddingTop(),
+                    systemBars.right + v.getPaddingRight(),
+                    systemBars.bottom + v.getPaddingBottom());
             return insets;
         });
         // ----------------------- onCreate -----------------------
+        initViews();
+        buttonLogin.setOnClickListener(v -> {
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+        });
+        textViewForgotPassword.setOnClickListener(v -> {
+            // TODO
+        });
+        textViewRegister.setOnClickListener(v -> {
+            // TODO
+        });
+    }
 
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewRegister = findViewById(R.id.textViewRegister);
     }
 }
