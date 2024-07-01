a64d0ca3c3e12d16e814fa50005f9871efaddf97
k8abadzheva <make.me.loco@gmail.com>
2024-07-01 03:03:22 +0300

    feat: Implemented logout functionality
    Added logout functionality to the app. Users can now log out from the UsersActivity using the options menu.
    
    - Added a logout
     option to the main menu.
    - Implemented logout logic in the UsersViewModel.
    - Updated UsersActivity to handle logout events and redirect to LoginActivity.
    - Removed unnecessary AuthStateListener in LoginViewModel.

--- app/src/main/java/com/abadzheva/messenger/UsersActivity.java
+++ app/src/main/java/com/abadzheva/messenger/UsersActivity.java
@@ -3,14 +3,20 @@ package com.abadzheva.messenger;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 public class UsersActivity extends AppCompatActivity {
+
+    private UsersViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,32 @@ public class UsersActivity extends AppCompatActivity {
             return insets;
         });
         // -------------------------------------------------
+        viewModel = new ViewModelProvider(this).get(UsersViewModel.class);
+        observeViewModel();
+    }
 
+    private void observeViewModel() {
+        viewModel.getUser().observe(this, firebaseUser -> {
+            if (firebaseUser == null) {
+                Intent intent = LoginActivity.newIntent(UsersActivity.this);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.item_logout) {
+            viewModel.logout();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static Intent newIntent(Context context) {
--- app/src/main/java/com/abadzheva/messenger/UsersViewModel.java
+++ app/src/main/java/com/abadzheva/messenger/UsersViewModel.java
@@ -8,31 +8,21 @@ import androidx.lifecycle.ViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginViewModel extends ViewModel {
+public class UsersViewModel extends ViewModel {
 
     private FirebaseAuth auth;
-
-    private MutableLiveData<String> error = new MutableLiveData<>();
     private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
 
-    public LoginViewModel() {
+    public UsersViewModel() {
         auth = FirebaseAuth.getInstance();
         auth.addAuthStateListener(firebaseAuth -> user.setValue(firebaseAuth.getCurrentUser()));
-    }
-
-    public LiveData<String> getError() {
-        return error;
     }
 
     public LiveData<FirebaseUser> getUser() {
         return user;
     }
 
-    public void login(String email, String password) {
-        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
-//            user.setValue(authResult.getUser());
-        }).addOnFailureListener(e -> {
-            error.setValue(e.getMessage());
-        });
+    public void logout() {
+        auth.signOut();
     }
 }
